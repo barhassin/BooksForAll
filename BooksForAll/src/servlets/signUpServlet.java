a7fed113fcbe27f45048b8ccfc4ebb28992ac1ad
yuval <yuvalzernik.yz@@gmail.com>
2018-02-11 00:14:10 +0200

    commit

---
+++ BooksForAll/src/servlets/signUpServlet.java
@@ -0,0 +1,108 @@
+package servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+import com.google.gson.Gson;
+
+import classes.loginValidator;
+import classes.AppConstants;
+import classes.User;
+
+/**
+ * Servlet implementation class signUpServlet
+ */
+@WebServlet(urlPatterns = { "/signUp"})
+public class signUpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public signUpServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Context context = new InitialContext();
+    		BasicDataSource ds = (BasicDataSource)context.lookup(
+    			getServletContext().getInitParameter(AppConstants.DB_DATASOURCE) + AppConstants.OPEN);
+    		Connection conn = ds.getConnection();
+    		BufferedReader reader = request.getReader();
+    	    StringBuilder sb = new StringBuilder();
+    	    String line = reader.readLine();
+    	    while (line != null) {
+    	      sb.append(line + "\n");
+    	      line = reader.readLine();
+    	    }
+    	    reader.close();
+    	    String params = sb.toString();
+    	    Gson gson = new Gson();
+    	    User user = gson.fromJson(params, User.class);
+    		PreparedStatement stmt;
+    			try {
+    				stmt = conn.prepareStatement(AppConstants.SELECT_USER_BY_USERNAME_STMT);
+    				stmt.setString(1, user.getUsername());
+    				ResultSet rs = stmt.executeQuery();
+    				String dbPassword="";
+    				if(rs.next()) {
+    					dbPassword=rs.getString(2);
+    					if(!dbPassword.equals(user.getPassword()))
+        					response.sendError(406);
+    				}	
+    				else {
+    					response.sendError(405);
+    				}
+    				rs.close();
+    				stmt.close();
+    			} catch (SQLException e) {
+    				getServletContext().log("Error while querying for customers", e);
+    	    		response.sendError(500);//internal server error
+    			}
+    		conn.close();
+    		
+    		Gson gson2 = new Gson();
+        	//convert from customers collection to json
+        	String userJsonResult = gson2.toJson(user);
+        	response.addHeader("Content-Type", "application/json");
+        	PrintWriter writer = response.getWriter();
+        	writer.println(userJsonResult);
+        	writer.close();
+		} catch (SQLException | NamingException e) {
+    		getServletContext().log("Error while closing connection", e);
+    		response.sendError(500);//internal server error
+    	}
+	}
+
+}
