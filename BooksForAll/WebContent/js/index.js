2c985c3e8af10b8e0ed99acc0fcc6d89f20a00a7
Hashi <hashi7ster@gmail.com>
2018-02-17 23:47:53 +0200

    why wont this end

--- BooksForAll/WebContent/js/index.js
+++ BooksForAll/WebContent/js/index.js
@@ -349,3 +349,15 @@ app.controller('viewPurchasesController', function($rootScope,$scope,$http,$wind
 app.controller('viewReviewsController', function($rootScope,$scope,$http,$window) {
 	
 });
+app.controller('payment', function($rootScope,$scope,$http,$window) {
+	//$scope.price = $rootscope.bookPrice;
+	$scope.threeNumbers = /^[0-9]{3}$/;
+	$scope.twoNumbers = /^[0-9][0-9]$/;
+	$scope.lettersOnly =/^[a-zA-Z ]{1,100}$/;
+	$scope.amexValidation = /^[3][4][0-9]{13}$/;
+	$scope.amexValidation = /^[4][0-9]{15}$/;
+
+	$scope.submitPaymet=function(){
+		
+	}
+});
